01688fea1e738d6029b55e420e76bedede6f6128
codernoyan <coder.noyan@gmail.com>
2023-02-19 18:08:44 +0600

    added redux and form events added

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// action buttons
-const incrementEl = document.getElementById('increment');
-const decrementEl = document.getElementById('decrement');
-
 // delete button
 const deleteButton = document.getElementById('delete-button');
 
@@ -10,3 +6,40 @@ const addAnotherButton = document.getElementById('add-another');
 
 // reset button
 const resetButton = document.getElementById('reset-button');
+
+// result
+const result = document.getElementById('result');
+
+document.getElementById('increaseForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  // action elements
+  const incrementValue = document.getElementById('increment').value;
+  const incrementValueInNumber = parseInt(incrementValue);
+  
+});
+document.getElementById('decreaseForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  // action elements
+  const decrementValue = document.getElementById('decrement').value;
+  const decrementValueInNumber = parseInt(decrementValue);
+});
+
+// initial state
+const initialState = {
+  matchCount: 0,
+}
+
+// counter reducer
+const matchCountReducer = (state = initialState, action) => {
+  
+}
+
+// Redux store
+const store = Redux.store();
+
+// render
+const render = () => {
+  const state = store.getState();
+};
+
+store.subscribe(render);
