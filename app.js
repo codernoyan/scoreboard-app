cb0090c7ed1283229cdcf2bb882666e592f3fdfc
codernoyan <coder.noyan@gmail.com>
2023-02-19 18:26:57 +0600

    added render function

--- app.js
+++ app.js
@@ -10,19 +10,23 @@ const resetButton = document.getElementById('reset-button');
 // result
 const result = document.getElementById('result');
 
-document.getElementById('increaseForm').addEventListener('submit', (e) => {
-  e.preventDefault();
-  // action elements
-  const incrementValue = document.getElementById('increment').value;
-  const incrementValueInNumber = parseInt(incrementValue);
-  
-});
-document.getElementById('decreaseForm').addEventListener('submit', (e) => {
-  e.preventDefault();
-  // action elements
-  const decrementValue = document.getElementById('decrement').value;
-  const decrementValueInNumber = parseInt(decrementValue);
-});
+// action identifier
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+
+// action creator
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  }
+}
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value,
+  }
+}
 
 // initial state
 const initialState = {
@@ -31,15 +35,45 @@ const initialState = {
 
 // counter reducer
 const matchCountReducer = (state = initialState, action) => {
-  
+  if (action.type === INCREMENT) {
+    return {
+      ...state,
+      matchCount: state.matchCount + action.payload,
+    }
+  } else if (action.type === DECREMENT) {
+    return {
+      ...state,
+      matchCount: state.matchCount - action.payload,
+    }
+  } else {
+    return state;
+  }
 }
 
 // Redux store
-const store = Redux.store();
+const store = Redux.createStore(matchCountReducer);
 
 // render
 const render = () => {
   const state = store.getState();
+  result.innerText = state.matchCount;
 };
 
+render();
+
 store.subscribe(render);
+
+
+document.getElementById('increaseForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  // action elements
+  const incrementValue = document.getElementById('increment').value;
+  const incrementValueInNumber = parseInt(incrementValue);
+
+});
+document.getElementById('decreaseForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  // action elements
+  const decrementValue = document.getElementById('decrement').value;
+  const decrementValueInNumber = parseInt(decrementValue);
+});
