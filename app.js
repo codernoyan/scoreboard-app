c20d990b658010ccdb596199ddcb9db10ab6b480
codernoyan <coder.noyan@gmail.com>
2023-02-19 17:09:38 +0600

    added id and catches id's with dom

---
+++ app.js
@@ -0,0 +1,12 @@
+// action buttons
+const incrementEl = document.getElementById('increment');
+const decrementEl = document.getElementById('decrement');
+
+// delete button
+const deleteButton = document.getElementById('delete-button');
+
+// add another 
+const addAnotherButton = document.getElementById('add-another');
+
+// reset button
+const resetButton = document.getElementById('reset-button');
